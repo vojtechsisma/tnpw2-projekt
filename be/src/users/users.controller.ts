1650bba9a1949f7b00c92a96123704b30a9757fb
Vojtěch Šišma <v.sisma@nextap.eu>
2024-04-10 22:39:17 +0200

    be: remove unused user EPs

--- be/src/users/users.controller.ts
+++ be/src/users/users.controller.ts
@@ -1,23 +1,21 @@
 import {
+  Req,
   Get,
   Post,
   Body,
   Patch,
-  Param,
   Delete,
   UseGuards,
   Controller,
-  ParseIntPipe,
-  Req,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
-import { UserEntity } from './entities/user.entity';
 import { UsersService } from './users.service';
+import { UserEntity } from './entities/user.entity';
+import { AuthenticateddRequest } from 'src/lib/types';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import { JwtStrategy } from 'src/auth/jwt.strategy';
 
 @ApiTags('Users')
 @Controller('users')
@@ -29,56 +27,31 @@ export class UsersController {
     return new UserEntity(await this.usersService.create(createUserDto));
   }
 
-  @Get()
-  @ApiBearerAuth()
-  @UseGuards(JwtAuthGuard)
-  async findAll(): Promise<UserEntity[]> {
-    const users = await this.usersService.findAll();
-    return users.map((user) => new UserEntity(user));
-  }
-
   @Get('/me')
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   async findAuthenticatd(
-    @Req() request: Request & { user: { id: number } },
+    @Req() request: AuthenticateddRequest,
   ): Promise<UserEntity> {
     return new UserEntity(await this.usersService.findOne(request.user.id));
   }
 
-  @Get(':id')
-  @ApiBearerAuth()
-  @UseGuards(JwtAuthGuard)
-  async findOne(@Param('id', ParseIntPipe) id: number): Promise<UserEntity> {
-    return new UserEntity(await this.usersService.findOne(id));
-  }
-
-  @Patch()
+  @Patch('/me')
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
   async updateSelf(
     @Body() updateUserDto: UpdateUserDto,
-    @Req() request: Request & { user: { id: number } },
+    @Req() request: AuthenticateddRequest,
   ): Promise<UserEntity> {
     return new UserEntity(
       await this.usersService.update(request.user.id, updateUserDto),
     );
   }
 
-  @Patch(':id')
+  @Delete('/me')
   @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
-  async update(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() updateUserDto: UpdateUserDto,
-  ): Promise<UserEntity> {
-    return new UserEntity(await this.usersService.update(id, updateUserDto));
-  }
-
-  @Delete(':id')
-  @ApiBearerAuth()
-  @UseGuards(JwtAuthGuard)
-  async remove(@Param('id', ParseIntPipe) id: number): Promise<UserEntity> {
-    return new UserEntity(await this.usersService.remove(id));
+  async remove(@Req() request: AuthenticateddRequest): Promise<UserEntity> {
+    return new UserEntity(await this.usersService.remove(request.user.id));
   }
 }
